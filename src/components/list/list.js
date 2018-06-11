85049344cea7abffcb33e140bda499222d996a62
Phil S <philipstubbs13@gmail.com>
2018-06-11 18:57:44 -0500

    ajax fetch and displaying state

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { handleResponse } from '../../helpers';
+import { API_URL } from '../../config';
+import Loading from '../common/Loading';
+import './Table.css';
 
 class List extends React.Component {
     constructor() {
@@ -7,19 +11,16 @@ class List extends React.Component {
         this.state = {
             loading: false,
             currencies: [],
-            error: null,
+            error: null
         };
     }
 
     componentDidMount() {
         this.setState({ loading: true });
 
-        fetch('https://api.udilia.com/coins/v1/cryptocurrencies?page=1&perPage=20')
-            .then(response => {
-                return response.json().then(json => {
-                    return response.ok ? json : Promise.reject(json);
-                });
-            })
+        fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
+            .then(handleResponse)
+
             .then((data) => {
                 // console.log('Success', data);
 
@@ -35,14 +36,61 @@ class List extends React.Component {
                 // console.log('Error', error);
             });
     }
+
+    renderChangePercent(percent) {
+        if (percent > 0) {
+            return <span className="percent-raised">{percent}% &uarr; </span>
+        } else if (percent < 0) {
+            return <span className="percent-fallen">{percent}% &darr;</span>
+        } else {
+            return <span>{percent}</span>
+        }
+    }
     render() {
-        console.log(this.state);
-        if (this.state.loading) {
-            return <div>Loading...</div>
+        const { loading, error, currencies } = this.state;
+        // console.log(this.state);
+        // render only loading component, if loading state is set to true.
+        if (loading) {
+            return <div className="loading-container"><Loading /></div>
+        }
+
+        // render only error message, if error occurred while fetching data.
+        if (error) {
+            return <div className="error">{error}</div>
         }
 
         return (
-            <div>text</div>
+            <div className="Table-container">
+                <table className="Table">
+                    <thead className="Table-head">
+                        <tr>
+                            <th>Cryptocurrency</th>
+                            <th>Price</th>
+                            <th>Market Cap</th>
+                            <th>24H Change</th>
+                        </tr>
+                    </thead>
+                    <tbody className="Table-body">
+                        {currencies.map((currency) => (
+                            <tr key={currency.id}>
+                                <td>
+                                    <span className="Table-rank">{currency.rank}</span> 
+                                    {currency.name}   
+                                </td>
+                                <td> 
+                                    <span>$ {currency.price} </span>
+                                </td>
+                                                                                <td> 
+                                    <span>$ {currency.marketCap} </span>
+                                </td>
+                                <td>   {this.renderChangePercent(currency.percentChange24h)}
+                                </td>
+                            </tr>
+                        ))}
+                    
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
---
+++ src/config.js
@@ -0,0 +1,2 @@
+// API root URL
+export const API_URL = 'https://api.udilia.com/coins/v1';

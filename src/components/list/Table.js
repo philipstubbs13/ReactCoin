aa166c34674ee666cbf191ab049bf058bb6c3560
Phil S <philipstubbs13@gmail.com>
2018-06-12 08:33:07 -0500

    routes

--- src/components/list/Table.js
+++ src/components/list/Table.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Table.css';
 
 const Table = (props) => {
-    const {currencies, renderChangePercent } = props;
+    const {currencies, renderChangePercent, history } = props;
     return (
         <div className="Table-container">
             <table className="Table">
@@ -17,7 +18,10 @@ const Table = (props) => {
                 </thead>
                 <tbody className="Table-body">
                     {currencies.map((currency) => (
-                        <tr key={currency.id}>
+                        <tr 
+                            key={currency.id}
+                            onClick={() => history.push(`/currency/${currency.id}`)}
+                            >
                             <td>
                                 <span className="Table-rank">{currency.rank}</span> 
                                 {currency.name}   
@@ -42,6 +46,7 @@ const Table = (props) => {
 Table.propTypes = {
     currencies: PropTypes.array.isRequired,
     renderChangePercent: PropTypes.func.isRequired,
+    history: PropTypes.object.isRequired,
 }
 
-export default Table;
+export default withRouter(Table);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import reactDom from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import List from './components/list/list';
+import NotFound from './components/notfound/NotFound';
+import Detail from './components/detail/Detail';
 import './index.css';
 
 const App = () => {
     const title = 'React Coin';
     return (
-        <div>
-            <Header />
-            <List />
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/" component={List} exact />
+                    <Route path="/currency/:id" component={Detail} exact />
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 

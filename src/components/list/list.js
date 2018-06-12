043ccdaec0144b7bbd9983bf2039bbe2decee3f1
Phil S <philipstubbs13@gmail.com>
2018-06-11 19:16:54 -0500

    props and proptypes checking

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
 import Loading from '../common/Loading';
-import './Table.css';
+import Table from './Table';
 
 class List extends React.Component {
     constructor() {
@@ -60,37 +60,9 @@ class List extends React.Component {
         }
 
         return (
-            <div className="Table-container">
-                <table className="Table">
-                    <thead className="Table-head">
-                        <tr>
-                            <th>Cryptocurrency</th>
-                            <th>Price</th>
-                            <th>Market Cap</th>
-                            <th>24H Change</th>
-                        </tr>
-                    </thead>
-                    <tbody className="Table-body">
-                        {currencies.map((currency) => (
-                            <tr key={currency.id}>
-                                <td>
-                                    <span className="Table-rank">{currency.rank}</span> 
-                                    {currency.name}   
-                                </td>
-                                <td> 
-                                    <span>$ {currency.price} </span>
-                                </td>
-                                                                                <td> 
-                                    <span>$ {currency.marketCap} </span>
-                                </td>
-                                <td>   {this.renderChangePercent(currency.percentChange24h)}
-                                </td>
-                            </tr>
-                        ))}
-                    
-                    </tbody>
-                </table>
-            </div>
+            <Table 
+                currencies={currencies}
+                renderChangePercent={this.renderChangePercent} />
         );
     }
 }
